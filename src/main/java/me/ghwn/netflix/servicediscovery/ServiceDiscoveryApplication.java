b082579764c9fddeb48abed44540fc61cec6fea6
Jihwan Lim <ghwn5936@gmail.com>
2021-10-11 00:32:15 +0900

    Initial commit

---
+++ src/main/java/me/ghwn/netflix/servicediscovery/ServiceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package me.ghwn.netflix.servicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDiscoveryApplication.class, args);
+    }
+
+}
